3d194ba2a3c65f5d0228c2740b93d20732480b90
Yusuf <yusuf.cakal@dolap.com>
2022-06-09 16:14:09 +0300

    refactor

--- src/main/java/com/codurance/base/StringCalculator.java
+++ src/main/java/com/codurance/base/StringCalculator.java
@@ -1,17 +1,15 @@
 package com.codurance.base;
 
+import java.util.Arrays;
+
 public class StringCalculator {
 
     public int add(String s) {
         if (s.isEmpty())
             return 0;
 
-        if(s.equals("1,2"))
-            return 3;
-
-        if(s.equals("3,5"))
-            return 8;
-
-        return Integer.parseInt(s);
+        return Arrays.stream(s.split(","))
+                .mapToInt(Integer::valueOf)
+                .sum();
     }
 }
