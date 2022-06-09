1d2f55831c76c03d96a8d846e5a79e9efff1a712
Yusuf <yusuf.cakal@dolap.com>
2022-06-09 15:24:33 +0300

    refactor

--- src/main/java/com/codurance/base/StringCalculator.java
+++ src/main/java/com/codurance/base/StringCalculator.java
@@ -3,6 +3,8 @@ package com.codurance.base;
 public class StringCalculator {
 
     public int add(String s) {
-        return 0;
+        if (s.isEmpty())
+            return 0;
+        return -1;
     }
 }
